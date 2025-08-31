241f5879954f35f7b49115fe7b632a0699aca1a2
baekhyun0824 <2668030377@qq.com>
2025-08-31 19:10:31 +0800

    Add files via upload

---
+++ 02 数据类型/字符串型.cpp
@@ -0,0 +1,20 @@
+/*
+#include<iostream>
+using namespace std;
+#include<string>//用C++风格字符串，要包含这个头文件
+
+int main() {
+
+	//1.C风格字符串
+	char str[] = "hello world";
+	cout << str << endl;
+
+	//2.C++风格字符串
+	string str2 = "hello world";
+	cout << str2 << endl;
+
+	system("pause");
+
+	return 0;
+}
+*/
