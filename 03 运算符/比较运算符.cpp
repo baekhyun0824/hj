82953ccad73e27faed1d7d5c460d09dda7d7a307
baekhyun0824 <2668030377@qq.com>
2025-09-03 21:21:02 +0800

    Add files via upload

---
+++ 03 运算符/比较运算符.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	//比较运算，判断真假，输出0或1
+	int a = 4;
+	int b = 56;
+	cout << (a == b) << endl;
+	cout << (a != b) << endl;
+	cout << (a < b) << endl;
+	cout << (a > b) << endl;
+	cout << (a <= b) << endl;
+	cout << (a >= b) << endl;
+	
+	system("pause");
+
+	return 0;
+}
---
+++ 03 运算符/赋值运算符.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	//¸³ÖµÔËËã
+	int a = 56;
+	a = 4;
+	cout << "a=" << a << endl;
+
+	a = 56;
+	a += 2;//a=a+2
+	cout << "a="<<a << endl;
+
+	a = 56;
+	a -= 2;//a=a-2
+	cout << "a=" << a << endl;
+
+	a = 56;
+	a *= 2;//a=a*2
+	cout << "a=" << a << endl;
+
+	a = 56;
+	a /= 2;//a=a/2
+	cout << "a=" << a << endl;
+
+	a = 56;
+	a %= 2;//a=a%2
+	cout << "a=" << a << endl;
+
+	system("pause");
+
+	return 0;
+}
