860a225fd08a2d27e780c88bdc687d5fb9d48858
baekhyun0824 <2668030377@qq.com>
2025-09-15 16:01:13 +0800

    Add files via upload

---
+++ 代码/08 结构体/结构体内存.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	struct MyStruct1
+	{
+		char a;		// 1
+		char b;		// 1
+		int c;		// 4
+	};
+	cout << "MyStruct1 : " << sizeof(MyStruct1) << endl; 
+	//输出结果为8，int需要从4的倍数开始
+	struct MyStruct2
+	{
+		char a;
+		int c;
+		char b;
+	};
+	cout << "MyStruct2 : " << sizeof(MyStruct2) << endl;
+	//输出结果为12，结构体的成员会按照其类型大小进行，边界对齐
+
+	return 0;
+}
